ede374e67179cb4fdbce9cbb4b6bdba82f7b14af
Elvio Toccalino <me@etoccalino.com>
2013-08-16 23:12:02 -0300

    individual post pages.

--- client/helpers/router.js
+++ client/helpers/router.js
@@ -1,3 +1,9 @@
 Meteor.Router.add({
-  '/': 'postsList'
+  '/': 'postsList',
+  '/posts/:_id': {
+    to: 'postPage',
+    and: function (id) {
+      Session.set('currentPostId', id);
+    }
+  }
 });
