093b865451e7d3f59bb47ffcc37c48722cd3ccfa
Elvio Toccalino <me@etoccalino.com>
2013-08-17 11:55:26 -0300

    added users config, modified publish to provide profile.

--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,12 @@
 Meteor.publish('posts', function () {
   return Posts.find();
 });
+
+Meteor.publish('userData', function () {
+  return Meteor.users.find({
+    _id: this.userId,
+    fields: {
+      profile: true,
+    }
+  });
+});
